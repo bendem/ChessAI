02dffdae9794a01ef6b122dd95412ffdcdc34270
bendem <online@bendem.be>
2014-05-24 16:22:10 +0200

    Simplify CoordinatesIterator

--- src/main/java/be/bendem/chess/iterators/CoordinatesIterator.java
+++ src/main/java/be/bendem/chess/iterators/CoordinatesIterator.java
@@ -10,7 +10,7 @@ import java.util.Iterator;
  */
 public class CoordinatesIterator implements Iterator<Coordinates> {
 
-    private Coordinates current;
+    private final Coordinates current;
     private final Direction direction;
 
     public CoordinatesIterator(Coordinates coordinates, Direction direction) {
@@ -20,32 +20,13 @@ public class CoordinatesIterator implements Iterator<Coordinates> {
 
     @Override
     public boolean hasNext() {
-        switch(direction) {
-            case Up:
-                return current.getY() > 0;
-            case Down:
-                return current.getY() < 7;
-            case Left:
-                return current.getX() > 0;
-            case Right:
-                return current.getX() < 7;
-            case LeftUp:
-                return current.getX() > 0 && current.getY() > 0;
-            case LeftDown:
-                return current.getX() > 0 && current.getY() < 7;
-            case RightUp:
-                return current.getX() < 7 && current.getY() > 0;
-            case RightDown:
-                return current.getX() < 7 && current.getY() < 7;
-            default:
-                throw new AssertionError("Unknown direction");
-        }
+        return !Coordinates.overflow(current, direction);
     }
 
     @Override
     public Coordinates next() {
-        current = new Coordinates(current.getX() + direction.getX(), current.getY() + direction.getY());
-        return current;
+        current.add(direction);
+        return current.clone();
     }
 
     @Override
